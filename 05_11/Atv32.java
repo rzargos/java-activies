18d49adcc595101fb2ff1657ee1e1e40d363268c
rzargos <raphael.zargos@gmail.com>
2022-11-05 01:36:40 -0300

    Add a activie 05/11

---
+++ 05_11/Atv32.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Atv32 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int matriz[][] = new int[2][2];
+        int soma;
+
+        for (int h = 0; h < 2; h++) {
+            for (int v = 0; v < 2; v++) {
+                System.out.println("Insira um valor inteiro");
+                matriz[h][v] = input.nextInt();
+
+            }
+        }
+        
+        for (int h = 0; h < 2; h++) {
+            soma = 0;
+            for (int v = 0; v < 2; v++) {
+                soma = soma + matriz[h][v];
+            }
+            System.out.println(soma);
+        }
+    }
+}
